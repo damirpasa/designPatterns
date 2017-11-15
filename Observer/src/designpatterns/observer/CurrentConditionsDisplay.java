8c585ff3055d0a5d4805cd9408c3c3f5b19f273d
Damir Pasagic <damirpasa@gmail.com>
2017-11-15 14:47:24 +0100

    implementing the subject interface in weatherData

---
+++ Observer/src/designpatterns/observer/CurrentConditionsDisplay.java
@@ -0,0 +1,12 @@
+package designpatterns.observer;
+
+public class CurrentConditionsDisplay {
+	
+	private float temperature;
+	private float humidity;
+	private Subject weatherData;
+	
+	public CurrentConditionsDisplay(Subject weatherData) {
+		
+	}
+}
